a27c82b311cc29a86286c0c09bc3284243fcb8d5
Sina Rostami <62883434+sina-sec@users.noreply.github.com>
2023-01-10 18:29:55 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ install_x-ui() {
             exit 1
         fi
         echo -e "detected x-ui The latest version of:${last_version},start installation"
-        wget -N --no-check-certificate -O /usr/local/*.tar.gz https://github.com/sina-sec/x-ui/archive/refs/tags/${last_version}.tar.gz
+        wget -N --no-check-certificate -O /usr/local/*.tar.gz https://github.com/sina-sec/x-ui/releases/${last_version}/.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}download x-ui Failed, please make sure your server is able to download Github document${plain}"
             exit 1
